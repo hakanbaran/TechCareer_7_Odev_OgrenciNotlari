f0e3e12e04794e3b047ca004e28f5de5d6c79ab2
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-05 00:42:44 +0300

    First Commit...

---
+++ TechCareer_7_Odev_OgrenciNotlari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechCareer_7_Odev_OgrenciNotlari
+//
+//  Created by Hakan Baran on 5.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
